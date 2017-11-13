6d4aec20965b9cd9bea597049ee8e646465c8186
j-da <32098148+j-da@users.noreply.github.com>
2017-11-13 09:36:59 +0100

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -17,8 +17,8 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 
 ## Uncomment this to disable output compression
 # $wgDisableOutputCompression = true;
-$wgSitename      = "Rhenus IT";
-$wgMetaNamespace = "Rhenus IT";
+$wgSitename      = "RENAME";
+$wgMetaNamespace = "RENAME";
 
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
